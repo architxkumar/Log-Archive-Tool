155be54695d45bd5742876ffdf2d31aab195e59f
Archit Kumar <architxkumar@proton.me>
2025-08-30 11:57:55 +0530

    feat: enhance script usage

--- log-archive.sh
+++ log-archive.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -euo pipefail
 
 if [ $# -ne 2 ]; then
     echo "Error: Invalid number of arguments"
+    exit 1
 fi
 
-# The directory to read the files from
-input_directory=$1
-# The directory to write the files to
-output_directory=$2
+# The directory to read the files from to be compressed
+input_directory=$(realpath $1)
+# The directory to write the tar file to to
+output_directory=$(realpath $2)
 
 # First we want to check if the supplied arguments are indeed directory path or not
 if [[ -f $input_directory ]]; then
@@ -21,17 +23,16 @@ fi
 
 # Once the arguments are validated, check for existance of direcory
 if [[ -d $input_directory ]]; then
-    echo "Directory exists: $input_directory"
+    echo "Source directory exists: $input_directory"
 else
-    echo "Directory doesn't exist: $input_directory"
-    echo "Creating directory..."
-    mkdir $input_directory
+    echo "Source directory doesn't exist: $input_directory"
+    exit 1
 fi
 
 if [[ -d $output_directory ]]; then
-    echo "Directory exists: $output_directory"
+    echo "Target directory exists: $output_directory"
 else
-    echo "Directory doesn't exist: $output_directory"
+    echo "Target directory doesn't exist: $output_directory"
     echo "Creating directory...."
     mkdir $output_directory
 fi
@@ -40,6 +41,5 @@ date_label=$(date +"%Y%m%d")
 time_label=$(date +"%H%M%S")
 name="logs_arhive_${date_label}_${time_label}.tar.gz"
 
-cd $input_directory
-tar -cf $name *
-mv $name $output_directory
+cd $output_directory
+tar -czf $name -C $input_directory .
